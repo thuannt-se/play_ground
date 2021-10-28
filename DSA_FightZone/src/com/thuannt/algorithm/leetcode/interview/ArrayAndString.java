7632c61cace1898efc1ae328e922215034298f80
thuannt-se <thuannt.se@gmail.com>
2021-10-28 14:29:50 +0700

    Update ArrayAndString.java

--- DSA_FightZone/src/com/thuannt/algorithm/leetcode/interview/ArrayAndString.java
+++ DSA_FightZone/src/com/thuannt/algorithm/leetcode/interview/ArrayAndString.java
@@ -221,16 +221,90 @@ public class ArrayAndString {
     
     protected static class IntersectionTwoArrays {
         
+        //my solution
         public static int[] intersect(int[] nums1, int[] nums2) {
-            return null;
+            int [] tmp1;
+            int [] tmp2;
+            if(nums1.length <= nums2.length) {
+                tmp1 = nums1;
+                tmp2 = nums2;
+            } else {
+                tmp1 = nums2;
+                tmp2 = nums1;
+            }
+            nums1 = nums2 = null;
+            int count = 0;
+            int i = 0;
+            int j = 0;
+            int n = 0;
+            int[] holder = new int[tmp1.length];
+            while(i < tmp1.length) {
+                while(j < tmp2.length && i < tmp1.length) {
+                    if(tmp1[i] == tmp2[j++]) {
+                        tmp2[j-1] = -99;
+                        holder[count] = tmp1[i++];
+                        count++; n = j-1; j = 0; 
+                    }
+                }
+                if(j >= tmp2.length) i++; j = 0;
+            }
+            int [] result = new int[count];
+            System.arraycopy(holder, 0, result, 0, count);
+            return result;
         }
+    }
+    // some best solution from leetcode
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int[] hash = new int[1001];
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i=0;i<nums1.length;i++){
+            hash[nums1[i]]++;
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(hash[nums2[i]]>0){
+                arr.add(nums2[i]);
+                hash[nums2[i]]--;
+            }
+        }
+        int[]ans = new int[arr.size()];
+        for(int i=0;i<ans.length;i++){
+            ans[i] = arr.get(i);
+        }
+        return ans;
+    }
+    //solution using map
+    public int[] intersectWithMap(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap();
+        // tally nums1 freq
+        for(int i : nums1) {
+            map.put(i, map.getOrDefault(i,0) + 1);
+        }
+        
+        // decrement from nums1 freq and build solution
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        for(int i : nums2) {
+            if(map.containsKey(i) && map.get(i) > 0) {
+                res.add(i);
+                map.put(i, map.get(i) - 1);
+            }
+        }
+        int[] sol = new int[res.size()];
+        int j = 0;
+        for(int i : res) {
+            sol[j] = i;
+            j++;
+        }
+        return sol;
     }
     
     public static void main(String[] args) {
-        int[] sums = {-1, -1, -2};
-        int a = SingleNumber.singleNumber(sums);
-        System.out.println(a);
-        System.out.println(Arrays.toString(sums));
-     }
+        int[] nums1 = { 61, 24, 20, 58, 95, 53, 17, 32, 45, 85, 70, 20, 83, 62, 35, 89, 5, 95, 12, 86, 58, 77, 30, 64,
+                46, 13, 5, 92, 67, 40, 20, 38, 31, 18, 89, 85, 7, 30, 67, 34, 62, 35, 47, 98, 3, 41, 53, 26, 66, 40, 54,
+                44, 57, 46, 70, 60, 4, 63, 82, 42, 65, 59, 17, 98, 29, 72, 1, 96, 82, 66, 98, 6, 92, 31, 43, 81, 88, 60,
+                10, 55, 66, 82, 0, 79, 11, 81 };
+        int[] nums2 = { 5, 25, 4, 39, 57, 49, 93, 79, 7, 8, 49, 89, 2, 7, 73, 88, 45, 15, 34, 92, 84, 38, 85, 34, 16, 6,
+                99, 0, 2, 36, 68, 52, 73, 50, 77, 44, 61, 48 };
+        System.out.println(Arrays.toString(IntersectionTwoArrays.intersect(nums1, nums2)));
+    }
     
 }
